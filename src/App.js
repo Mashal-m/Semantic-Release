b166f052a2312f0c1de4150c65cc9bcec0647f69
mashal-m <mashal.malik@arbisoft.com>
2023-02-27 18:45:38 +0500

    edit message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Semantic Release Testingg
+       Semantic Release Testing for triggering dry run
       </header>
     </div>
   );
