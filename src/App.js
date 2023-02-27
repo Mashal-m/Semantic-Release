b2e7c805fab1b65bdcd6f898f51a5e0741bd7f24
mashal-m <mashal.malik@arbisoft.com>
2023-02-27 18:42:59 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Semantic Release Testing
+       Semantic Release Testingg
       </header>
     </div>
   );
